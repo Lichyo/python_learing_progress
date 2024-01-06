3613ad11aa4a5355e98765e888686b1bac3d0168
Lichyo <lichyo003@gmail.com>
2024-01-06 23:32:19 +0800

    sending email

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import requests
-from bs4 import BeautifulSoup
+import smtplib
 
-response = requests.get('https://www.billboard.com/charts/hot-100/')
-soup = BeautifulSoup(response.text, 'html.parser')
-hit_song_titles = [title.get_text() for title in soup.select('h3')]
-print(hit_song_titles)
+with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+    connection.starttls()
+    result = connection.login('lichyo003@gmail.com', 'ufku seeg iraa vlir')
+    connection.sendmail(
+        from_addr='lichyo003@gmail.com',
+        to_addrs='lichyo003@gmail.com',
+        msg=f"Testing if I could send email from python".encode("utf-8")
+    )
