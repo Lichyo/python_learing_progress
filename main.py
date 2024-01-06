814c609fa84c3c12006b4068309e10a2786ac9de
Lichyo <lichyo003@gmail.com>
2024-01-06 23:10:33 +0800

    scrap billboard unFinished

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get('https://www.empireonline.com/tv/features/best-tv-2023/')
+response = requests.get('https://www.billboard.com/charts/hot-100/')
 soup = BeautifulSoup(response.text, 'html.parser')
-titles = [title.text for title in soup.select('h2 strong')]
-titles = titles[::-1]   # reverse the array
-
-with open('movie_title.txt', 'a') as file:
-    for title in titles:
-        file.write(title)
-        file.write('\n')
+hit_song_titles = [title.get_text() for title in soup.select('h3')]
+print(hit_song_titles)
